96e792a1840d177502f3b8b01128f514ec4c358c
Nao YONASHIRO <owan.orisano@gmail.com>
2020-04-25 21:30:43 +0900

    feat: replace github.com/pkg/errors to golang.org/x/xerrors

--- main.go
+++ main.go
@@ -14,20 +14,20 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/pkg/errors"
+	"golang.org/x/xerrors"
 )
 
 func parseFile(path string) (*ast.File, error) {
 	f, err := os.Open(path)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to open file")
+		return nil, xerrors.Errorf("open file: %w", err)
 	}
 	defer f.Close()
 	return parser.ParseFile(token.NewFileSet(), path, f, 0)
 }
 
 func run() error {
-	fileName := flag.String("f", os.Getenv("GOFILE"), "target file")
+	fileName := flag.String("f", os.Getenv("GOFILE"), "target file (default $GOFILE)")
 	dirName := flag.String("d", "", "target directory")
 	outputName := flag.String("o", "", "output filename")
 
@@ -35,11 +35,11 @@ func run() error {
 
 	if *fileName == "" && *dirName == "" {
 		flag.Usage()
-		return errors.New("require -f or -d")
+		return xerrors.New("require -f or -d")
 	}
 	if *fileName != "" && *dirName != "" {
 		flag.Usage()
-		return errors.New("either -f or -d, not both")
+		return xerrors.New("either -f or -d, not both")
 	}
 
 	var fileNames []string
@@ -49,7 +49,7 @@ func run() error {
 	case *dirName != "":
 		infoList, err := ioutil.ReadDir(*dirName)
 		if err != nil {
-			return errors.Wrap(err, "failed to read dir")
+			return xerrors.Errorf("read dir: %w", err)
 		}
 		for _, info := range infoList {
 			name := info.Name()
@@ -64,7 +64,7 @@ func run() error {
 	if *outputName != "" {
 		f, err := os.Create(*outputName)
 		if err != nil {
-			return errors.Wrap(err, "failed to create file")
+			return xerrors.Errorf("create file: %w", err)
 		}
 		defer f.Close()
 		w = f
